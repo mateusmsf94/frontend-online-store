60563195873173ab75bc4c7ec3358d2c5e6172ef
Pedro Pugas <pedropugas@outlook.com.br>
2022-10-07 18:23:50 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Categories from './components/Categories';
 
 class App extends React.Component {
   render() {
     return (
-      // <>
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
-      // </>
+      <>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Categories />
+      </>
     );
   }
 }
