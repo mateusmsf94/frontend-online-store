f4402c2014413836590ef808598d71839dbede91
Wellington B <barbosadecarvalhow@gmail.com>
2022-10-07 17:31:30 -0300

    shoppingCart part-2

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <h1>Carrinho de compras</h1>
+    );
+  }
+}
+
+export default ShoppingCart;
